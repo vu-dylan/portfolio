a547b6015ba8003c57341509ac7eb94df6c15a2f
Dylan Vu <dylanvu92gmail.com>
2022-01-13 23:27:15 -0800

    add head

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import styles from '../styles/Main.module.css'
+import Head from 'next/head'
 import {useEffect} from 'react'
 import Header from "../components/Header";
 import ProjectGroup from "../components/ProjectGroup";
@@ -39,6 +40,18 @@ export default function Home() {
   return (
 
   <div>
+    <Head>
+      <meta charset="utf-8" />
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="height=device-height, width=device-width, initial-scale=1.0, minimum-scale=1"/>
+      <meta name="description" content="Dylan Vu's Website and Portfolio. Dylan Vu is a student at the University of California, Santa Barbra, and is an aspiring Software Developer. Dylan Vu is familiar with NodeJS and Python. He is interning at Ansync Labs as a Software Developer Intern."/>
+      <meta name="keywords" content="Dylan Vu, Website, Portfolio, Software, Developer, Hackathon, Engineer, NodeJS, Programming, Python, UCSB"/>
+      <meta name="theme-color" content="#000000" />
+      <meta name="google-site-verification" content="sUS_xDwTE-3CQEL_hjTnmk-DzsGo1-3mEprUz09L84Y" />
+      <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+      <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+      <title>Dylan Vu | Website Portfolio | NodeJS, Python</title>
+    </Head>
     <Header/>
     <Navbar />
     <div className={styles.content}>
